d86907030d4b1a863912c259f0638fa62082db38
Adr <152195399+acsu132@users.noreply.github.com>
2025-02-23 13:10:30 -0300

    Made setTicketChannel compatible with ticket handler
    Also added an category option to it

--- commands/setups/setTicketChannel.js
+++ commands/setups/setTicketChannel.js
@@ -1,24 +1,3 @@
-/*
-
-☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
-                                                 
-  _________ ___ ___ ._______   _________    
- /   _____//   |   \|   \   \ /   /  _  \   
- \_____  \/    ~    \   |\   Y   /  /_\  \  
- /        \    Y    /   | \     /    |    \ 
-/_______  /\___|_  /|___|  \___/\____|__  / 
-        \/       \/                     \/  
-                    
-DISCORD :  https://discord.com/invite/xQF9f9yUEM                   
-YouTube : https://www.youtube.com/@GlaceYT                         
-
-Command Verified : ✓  
-Website        : ssrr.tech  
-Test Passed    : ✓
-
-☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
-*/
-
 const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require('discord.js');
 const { ticketsCollection } = require('../../mongodb');
 const cmdIcons = require('../../UI/icons/commandicons');
@@ -49,6 +28,10 @@ module.exports = {
                     option.setName('status')
                         .setDescription('Enable or disable the ticket system')
                         .setRequired(true))
+                .addStringOption(option =>
+                    option.setName('categoryid')
+                        .setDescription('The ID of the category for the ticket channel')
+                        .setRequired(false))
         )
 
         // View ticket channel configuration
@@ -63,49 +46,13 @@ module.exports = {
 
     async execute(interaction) {
         if (interaction.isCommand && interaction.isCommand()) {
-           
+            const subcommand = interaction.options.getSubcommand();
+            const serverId = interaction.options.getString('serverid');
+            const guild = interaction.guild;
 
-        const subcommand = interaction.options.getSubcommand();
-        const serverId = interaction.options.getString('serverid');
-        const guild = interaction.guild;
-
-        if (serverId !== guild.id) {
-            return interaction.reply({ content: 'The server ID provided does not match this server.', flags: 64 });
-        }
-
-            const configMangerData = await serverConfigCollection.findOne({ serverId });
-                const botManagers = configMangerData ? configMangerData.botManagers || [] : [];
-      
-                if (!botManagers.includes(interaction.user.id) && interaction.user.id !== guild.ownerId) {
-                    return interaction.reply({ 
-                        content: '❌ Only the **server owner** or **bot managers** can use this command.', 
-                        flags: 64
-                    });
-                }
-
-        const configData = await ticketsCollection.findOne({ serverId });
-
-        if (subcommand === 'set') {
-            const channelId = interaction.options.getString('channelid');
-            const adminRoleId = interaction.options.getString('adminroleid');
-            const status = interaction.options.getBoolean('status');
-
-            if (!serverId || !channelId || !adminRoleId || status === null) {
-                return interaction.reply({ content: 'Invalid input. Please provide valid server ID, channel ID, admin role ID, and status.', flags: 64 });
+            if (serverId !== guild.id) {
+                return interaction.reply({ content: 'The server ID provided does not match this server.', flags: 64 });
             }
-
-            // Check permissions: Only the server owner or assigned owners can modify settings.
-            const serverOwnerId = guild.ownerId;
-            // Update the database
-            await ticketsCollection.updateOne(
-                { serverId },
-                { $set: { serverId, ticketChannelId: channelId, adminRoleId, status, ownerId: serverOwnerId } },
-                { upsert: true }
-            );
-
-            return interaction.reply({ content: `Ticket channel updated successfully for server ID ${serverId}.`, flags: 64 });
-
-        } else if (subcommand === 'view') {
 
             const configMangerData = await serverConfigCollection.findOne({ serverId });
             const botManagers = configMangerData ? configMangerData.botManagers || [] : [];
@@ -116,58 +63,64 @@ module.exports = {
                     flags: 64
                 });
             }
-            if (!configData) {
-                return interaction.reply({ content: 'No ticket system configuration found for this server.', flags: 64 });
-            }
 
-            // Create an embed with server configuration details
+            const configData = await ticketsCollection.findOne({ serverId });
+
+            if (subcommand === 'set') {
+                const channelId = interaction.options.getString('channelid');
+                const adminRoleId = interaction.options.getString('adminroleid');
+                const status = interaction.options.getBoolean('status');
+                const categoryId = interaction.options.getString('categoryid');
+
+                if (!serverId || !channelId || !adminRoleId || status === null) {
+                    return interaction.reply({ content: 'Invalid input. Please provide valid server ID, channel ID, admin role ID, and status.', flags: 64 });
+                }
+
+                // Check permissions: Only the server owner or assigned owners can modify settings.
+                const serverOwnerId = guild.ownerId;
+                // Update the database
+                await ticketsCollection.updateOne(
+                    { serverId },
+                    { $set: { serverId, ticketChannelId: channelId, adminRoleId, status, categoryId, ownerId: serverOwnerId } },
+                    { upsert: true }
+                );
+
+                return interaction.reply({ content: `Ticket channel updated successfully for server ID ${serverId}.`, flags: 64 });
+
+            } else if (subcommand === 'view') {
+
+                if (!configData) {
+                    return interaction.reply({ content: 'No ticket system configuration found for this server.', flags: 64 });
+                }
+
+                // Create an embed with server configuration details
+                const embed = new EmbedBuilder()
+                    .setColor('#3498db')
+                    .setTitle('Ticket System Configuration')
+                    .setDescription(`
+                        **Server ID:** ${configData.serverId}
+                        **Ticket Channel ID:** ${configData.ticketChannelId}
+                        **Admin Role ID:** ${configData.adminRoleId}
+                        **Status:** ${configData.status ? 'Enabled' : 'Disabled'}
+                        **Category ID:** ${configData.categoryId || 'None'}
+                    `)
+                    .setTimestamp();
+
+                return interaction.reply({ embeds: [embed], flags: 64 });
+            }
+        } else {
+            // Not a slash command usage.
             const embed = new EmbedBuilder()
                 .setColor('#3498db')
-                .setTitle('Ticket System Configuration')
-                .setDescription(`
-                    **Server ID:** ${configData.serverId}
-                    **Ticket Channel ID:** ${configData.ticketChannelId}
-                    **Admin Role ID:** ${configData.adminRoleId}
-                    **Status:** ${configData.status ? 'Enabled' : 'Disabled'}
-                `)
+                .setAuthor({
+                    name: "Alert!",
+                    iconURL: cmdIcons.dotIcon,
+                    url: "https://discord.gg/xQF9f9yUEM"
+                })
+                .setDescription('- This command can only be used through slash commands!\n- Please use `/setticketchannel`')
                 .setTimestamp();
 
-            return interaction.reply({ embeds: [embed], flags: 64 });
+            return interaction.reply({ embeds: [embed] });
         }
-    } else {
-        // Not a slash command usage.
-        const embed = new EmbedBuilder()
-            .setColor('#3498db')
-            .setAuthor({
-                name: "Alert!",
-                iconURL: cmdIcons.dotIcon,
-                url: "https://discord.gg/xQF9f9yUEM"
-            })
-            .setDescription('- This command can only be used through slash commands!\n- Please use `/setticketchannel`')
-            .setTimestamp();
-
-        return interaction.reply({ embeds: [embed] });
-    }
     }
 };
-
-/*
-
-☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
-                                                 
-  _________ ___ ___ ._______   _________    
- /   _____//   |   \|   \   \ /   /  _  \   
- \_____  \/    ~    \   |\   Y   /  /_\  \  
- /        \    Y    /   | \     /    |    \ 
-/_______  /\___|_  /|___|  \___/\____|__  / 
-        \/       \/                     \/  
-                    
-DISCORD :  https://discord.com/invite/xQF9f9yUEM                   
-YouTube : https://www.youtube.com/@GlaceYT                         
-
-Command Verified : ✓  
-Website        : ssrr.tech  
-Test Passed    : ✓
-
-☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
-*/
